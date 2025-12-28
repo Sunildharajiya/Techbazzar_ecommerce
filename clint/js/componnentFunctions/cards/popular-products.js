529d6750b07aa1244f7684e5170377ca4e14e1e5
Sunildharajiya <sunildharajiyablack@gmail.com>
2025-12-28 22:45:31 +0530

    mini css changes

--- clint/js/componnentFunctions/cards/popular-products.js
+++ clint/js/componnentFunctions/cards/popular-products.js
@@ -7,7 +7,25 @@ let Popproducjson = [
   shortdiscription:"this is very nice laptop",
     longdiscription:"hdhdjhsjbdjksbhajhshshshbsvsvsbxhxnbxhjsbdjxh hjsbdhksb hsnshskskshs jjbsbhsokdj jksbdjksneik iebhsjsbhsisnd   jsbsohsvskdoifb jsjisjjdjdjjd",
     price:20990,
-    mainimgurl:"hsjvdjxbvxbzjsijs",
+    mainimgurl:`./assets/banners/banner1.jpg`,
+    subimgs:{
+      img1:"hdjdjjdjdkdjhsiwbbskkshsmsbg",
+      img2:"hdjdjjdjdkdjhsiwbbskkshsmsbg",
+      img3:"hdjdjjdjdkdjhsiwbbskkshsmsbg"
+    },
+    reviews:{
+      r1:"hi how are you ?",
+      r2:"hi how are you ?",
+      r3:"hi how are you ?",
+    }
+  },
+  {
+   id:1,
+   name:"intel thinkpad i5",
+  shortdiscription:"this is very nice laptop",
+    longdiscription:"hdhdjhsjbdjksbhajhshshshbsvsvsbxhxnbxhjsbdjxh hjsbdhksb hsnshskskshs jjbsbhsokdj jksbdjksneik iebhsjsbhsisnd   jsbsohsvskdoifb jsjisjjdjdjjd",
+    price:20990,
+    mainimgurl:`./assets/banners/banner1.jpg`,
     subimgs:{
       img1:"hdjdjjdjdkdjhsiwbbskkshsmsbg",
       img2:"hdjdjjdjdkdjhsiwbbskkshsmsbg",
@@ -24,10 +42,16 @@ let Popproducjson = [
   
   console.log(Popproducjson)
   
-  function crearteCard(e){
+  function crearteCards(e){
     let div = document.createElement('div')
-    div.innerHTML = `<h1>hi</hi>`
+    div.className = "popular-cards"
+    div.innerHTML = `<img src=${e.mainimgurl} alt = "not found img" class = "card-img">
+    <h4>${e.name}</h4>
+    <p>${e.shortdiscription}</p>
+    <span>${e.price} ₹</span>
+    <button>BUY NOW</button>
+    ` 
     popularProductsBox.appendChild(div)
   }
   
-  crearteCard()
+  Popproducjson.forEach((e) => crearteCards(e))
